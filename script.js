4800f6c88b915bfea82fbf8a2f5b0a65aeb73ad5
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-11-10 17:32:42 +0100

    Add pictures

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const load = document.getElementById('download');
+const info = document.querySelector('.info')
+const blockAmount = 5;
 
-const loadInfo = () => {
+const loadInfo = async () => {
+  let infoBlock = ``;
 
+  for(let i = 0; i < blockAmount; i++) {
+    let response = await fetch("https://randomuser.me/api").then(res => res.json());
+    let picture = response.results[0].picture.large;
+
+    infoBlock += `
+      <div class = "info-block">
+        <img src=${picture}>
+      </div>
+    `;
+  }
+
+  info.innerHTML = infoBlock;
 }
 
 load.addEventListener('click', loadInfo);
